420958975dc3b5558927a175e28826daa5e8f2da
CommieGIR <KrausEngineering@gmail.com>
2020-06-06 20:30:11 -0400

    Update M5StackCoreGeiger.ino

--- M5StackCoreGeiger.ino
+++ M5StackCoreGeiger.ino
@@ -5,6 +5,8 @@ Project Includes Code from Radiation Watch
 https://github.com/MonsieurV/ArduinoPocketGeiger
 Also includes M5Stack Code from
 https://github.com/m5stack/M5Stack
+You will need to go into your RadiationWatch.h file in C:\Users\YOURNAMEHERE\Documents\Arduino\libraries\RadiationWatch\src
+And change pins, as Pin 3 will not provide good resolution on the ESP32 in the M5Stack.
 /*signPin = 2, noisePin = 5*/
 
 RadiationWatch radiationWatch;
